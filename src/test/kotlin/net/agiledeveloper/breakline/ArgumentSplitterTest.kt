ff7e1d0e2645b5e0540187608c7d8a2fb5759bdd
Julien Fischer <julien.fischer@agiledeveloper.net>
2025-02-24 00:03:53 +0100

    Add MIT LICENSE

--- src/test/kotlin/net/agiledeveloper/breakline/ArgumentSplitterTest.kt
+++ src/test/kotlin/net/agiledeveloper/breakline/ArgumentSplitterTest.kt
@@ -1,3 +1,7 @@
+// This file is licensed under the MIT License.
+// See the LICENSE file in the project root for more information:
+// https://github.com/Julien-Fischer/break-line/blob/main/LICENSE
+
 package net.agiledeveloper.breakline
 
 import net.agiledeveloper.breakline.splitters.ArgumentSplitter
